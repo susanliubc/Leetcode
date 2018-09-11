c87e651a1f71ad80beb4397e2eeb22663a5677b2
susanliubc <susanliubc@gmail.com>
2018-09-11 16:33:13 -0700

    add Q119

--- Leetcode_Array.js
+++ Leetcode_Array.js
@@ -111,3 +111,32 @@ var plusOne = function(digits) {
     digits.unshift(1);
     return digits;
 };
+
+// 119. Pascal's Triangle II
+// Given a non-negative index k where k ≤ 33, return the kth index row of the Pascal's triangle.
+
+// Note that the row index starts from 0.
+// In Pascal's triangle, each number is the sum of the two numbers directly above it.
+
+// Example:
+
+// Input: 3
+// Output: [1,3,3,1]
+
+// Follow up:
+
+// Could you optimize your algorithm to use only O(k) extra space?
+
+//sample 48 ms submission 
+var getRow = function(rowIndex) {
+    var result = [];
+    result[0] = 1;
+    for(var i = 1 ; i < rowIndex+1 ; i++){            
+        if(i > 1){
+            for(var j = i-1 ; j > 0 ; j--)
+                result[j] = result[j]+result[j-1];
+        }
+        result.push(1);
+    }
+    return result;
+};
