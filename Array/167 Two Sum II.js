e59c48db68e4b5ccd49f9b76b6d6f2c7edaef13b
susanliubc <susanliubc@gmail.com>
2018-09-12 15:22:44 -0700

    change comment style

--- Array/167 Two Sum II.js
+++ Array/167 Two Sum II.js
@@ -1,19 +1,19 @@
-// Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+/* Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
 
-// The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
+The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
 
-// Note:
+Note:
 
-//     Your returned answers (both index1 and index2) are not zero-based.
-//     You may assume that each input would have exactly one solution and you may not use the same element twice.
+     Your returned answers (both index1 and index2) are not zero-based.
+     You may assume that each input would have exactly one solution and you may not use the same element twice.
 
-// Example:
+Example:
 
-// Input: numbers = [2,7,11,15], target = 9
-// Output: [1,2]
-// Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
 
-//sample 52 ms submission
+sample 52 ms submission */
 var twoSum = function(numbers, target) {
     let i=0, j=numbers.length - 1;
     let sum = 0;
